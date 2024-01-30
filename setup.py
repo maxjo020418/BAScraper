29d1459dd38a730121789100bef0b9cb9bc936fd
maxjo <maxjo020418@gmail.com>
2024-01-31 01:26:15 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'API wrapper for PullPush.io - the 3rd party replacement API for Reddit.'
 LONG_DESCRIPTION = '''currently it can:
 - get submissions/comments from a certain subreddit in supported order/sorting methods specified in the PullPush.io API docs
 - get comments under the retrieved submissions
 - can get all the submissions based on the number of submissions or in a certain timeframe you specify
-- can recover(if possible) deleted/removed submission/comments from the returned result'''
+- can recover(if possible) deleted/removed submission/comments from the returned result
+check the [documentation on the github](https://github.com/maxjo020418/BAScraper) for detailed info
+'''
 
 # Setting up
 setup(
@@ -18,15 +20,19 @@ setup(
     author="maxjo",
     author_email="jo@yeongmin.net",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['requests'],
 
-    keywords=['reddit scraper', 'reddit', 'pullpush', 'wrapper'],
+    keywords=['reddit scraper', 'reddit', 'scraper', 'pullpush', 'wrapper'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
     ],
-    python_requires=">=3.8"
+    python_requires=">=3.8",
+    project_urls={
+      'Github': 'https://github.com/maxjo020418/BAScraper'
+    }
 )
